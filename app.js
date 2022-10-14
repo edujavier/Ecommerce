09618bd7ab14bcd474b6b5afde7448708ed9f186
edujavier <delpozoeduardo@hotmail.com>
2022-10-13 20:42:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const carShop1 = document.querySelector('.cartShop1');
+const Carrito = document.querySelector('.car');
+const modalContainer = document.getElementById("modal-container");
+
+/*productos = [
+  
+  {
+    id: 1,
+    nombre: 'Nike Air 1',
+    precio: 78,
+    img:
+    src="./assets/blazer.png"
+  },
+  {
+    id: 2,
+    nombre: 'Nike Air 2',
+    precio: 78,
+    img:
+    src="./assets/air2.png"
+  },
+  {
+    id: 3,
+    nombre: 'Nike Jordan',
+    precio: 78,
+    img:
+    src="./assets/jordan.png"
+  },
+
+];*/
+
+//let carrito = [];
+/*//recorro todo el arreglo de productos
+productos.forEach((product) => {
+  let content = document.createElement("div");//creamos un div para los productos
+  content.innerHTML =`
+  <img src="${product.img}">
+  <h3>${product.nombre}</h3>
+  <p>${product.precio} $</p>
+  `;
+
+  
+});*/
+/*const carrito = document.querySelectorAll('.cartShop')
+carrito.addEventListener('click', () => {
+ console.log('revisado')
+})*/
+
+/*carShop1.addEventListener('click' ,() =>{
+  
+});*/
+/*Carrito.addEventListener('click', () =>{
+  /*console.log('hola')*/
+  /*const modalHeader = document.createElement("div");
+  modalHeader.className = 'modal-header';
+  modalHeader.innerHTML=`
+  <h1 class="modal-header-title">Carrito.</h1>
+  `;
+  modalContainer.append(modalHeader);
+
+  const modalButton = document.createElement("h1");
+  modalButton.innerText = "x";
+  modalButton.className = "modal-header-button";
+
+  modalHeader.append(modalButton);*/
+  
+
+
+
